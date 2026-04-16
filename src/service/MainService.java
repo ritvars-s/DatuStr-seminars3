af61bb6e99a876ceeb483c40ac8c6ab4328995db
Ritvars <ritvarsbaders@gmail.com>
2026-04-16 10:12:45 +0300

    Added MyNode class

--- src/service/MainService.java
+++ src/service/MainService.java
@@ -1,7 +1,9 @@
 package service;
-
+//leftchindex = parentindex * 2 + 1
+//rightchindex = parentindex * 2 + 2
+//parentindex = (leftchindex -1) / 2
+//parentindex = (rightchindex - 2) / 2
 public class MainService {
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
